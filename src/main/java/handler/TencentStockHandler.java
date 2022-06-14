a46222dbda37986a22991ba2e43e32f761ad46d0
liangjiedong <liangjiedong@yingzi.com>
2022-06-15 00:31:08 +0800

    1、插件修改

--- src/main/java/handler/TencentStockHandler.java
+++ src/main/java/handler/TencentStockHandler.java
@@ -96,6 +96,32 @@ public class TencentStockHandler extends StockRefreshHandler {
             bean.setTime(values[30]);
             bean.setMax(values[33]);//33
             bean.setMin(values[34]);//34
+            bean.setExchange(values[39]);
+
+            bean.setBuy1( values[10]);
+            bean.setBuy2( values[12]);
+            bean.setBuy3( values[14]);
+            bean.setBuy4( values[16]);
+            bean.setBuy5( values[18]);
+
+            bean.setSell1( values[20]);
+            bean.setSell2( values[22]);
+            bean.setSell3( values[24]);
+            bean.setSell4( values[26]);
+            bean.setSell5( values[28]);
+
+            bean.setBuyPrice1(values[9] );
+            bean.setBuyPrice2(values[11]) ;
+            bean.setBuyPrice3(values[13]) ;
+            bean.setBuyPrice4(values[15]) ;
+            bean.setBuyPrice5(values[17]) ;
+
+            bean.setSellPrice1(values[19]);
+            bean.setSellPrice2(values[21]);
+            bean.setSellPrice3(values[23]);
+            bean.setSellPrice4(values[25]);
+            bean.setSellPrice5(values[27]);
+
             updateData(bean);
         }
     }
--- src/main/java/utils/WindowUtils.java
+++ src/main/java/utils/WindowUtils.java
@@ -13,6 +13,7 @@ public class WindowUtils {
     public static final String FUND_TABLE_HEADER_VALUE = "编码,基金名称,估算净值,估算涨跌,更新时间,当日净值";
     //股票表头
     public static final String STOCK_TABLE_HEADER_KEY = "stock_table_header_key";
+    //public static final String STOCK_TABLE_HEADER_VALUE = "编码,股票名称,当前价,涨跌,涨跌幅,最高价,最低价,更新时间";
     public static final String STOCK_TABLE_HEADER_VALUE = "编码,股票名称,当前价,涨跌,涨跌幅,最高价,最低价,更新时间";
     //货币表头
     public static final String COIN_TABLE_HEADER_KEY = "coin_table_header_key";
